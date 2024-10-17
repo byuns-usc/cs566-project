354aeaffb0a695295a4060e32bc3e327b002d2a0
Kayvan Shah <59110083+KayvanShah1@users.noreply.github.com>
2024-10-17 12:38:34 -0700

    Refactor import statements for segone package

--- segone/networks/seg_decoder.py
+++ segone/networks/seg_decoder.py
@@ -21,18 +21,20 @@ class SegDecoder(nn.Module):
 
         # Initialize Layers
         self.convs = nn.ModuleDict()
-        for i in range(self.len_ch_enc-1, 0, -1):
+        for i in range(self.len_ch_enc - 1, 0, -1):
             self.convs[f"channel_1_{i}"] = Conv1DBlock(
                 kernel_size=self.channel_enc[i], out_channels=2 * self.channel_enc[i], stride=self.channel_enc[i]
             )
             self.convs[f"channel_2_{i}"] = Conv1DBlock(
-                kernel_size=2 * self.channel_enc[i-1], out_channels=2 * self.channel_enc[i-1], stride=2 * self.channel_enc[i-1]
+                kernel_size=2 * self.channel_enc[i - 1],
+                out_channels=2 * self.channel_enc[i - 1],
+                stride=2 * self.channel_enc[i - 1],
             )
-            self.convs[f"spatial_{i}"] = Conv1DBlock(
-                kernel_size=self.opts["kernel_size"]
-            )
+            self.convs[f"spatial_{i}"] = Conv1DBlock(kernel_size=self.opts["kernel_size"])
             self.convs[f"head_{i}"] = Conv1DBlock(
-                kernel_size=2 * self.channel_enc[i-1], out_channels=self.opts["channel_out"], stride=2 * self.channel_enc[i-1]
+                kernel_size=2 * self.channel_enc[i - 1],
+                out_channels=self.opts["channel_out"],
+                stride=2 * self.channel_enc[i - 1],
             )
 
         self.upsample = PixelShuffle(scale=2)
@@ -43,18 +45,14 @@ class SegDecoder(nn.Module):
 
         x = features_enc[-1]
         x, _ = spatial_flatten(x)
-        for i in range(self.len_ch_enc-1, -1, -1):
+        for i in range(self.len_ch_enc - 1, -1, -1):
             x = self.convs[f"channel_1_{i}"](x)
             x = channel_recover(x, h, w)
             x = self.upsample(x)
-            x, (_,_,h,w) = spatial_flatten(x)
+            x, (_, _, h, w) = spatial_flatten(x)
             x = self.convs[f"channel_2_{i}"](x)
             x = channel_flatten(x)
-            self.outputs.insert(0, 
-                self.channel_recover(
-                    self.convs[f"head_{i}"](x), h, w
-                )
-            )
+            self.outputs.insert(0, self.channel_recover(self.convs[f"head_{i}"](x), h, w))
             x = self.convs[f"spatial_{i}"](x)
 
         return self.outputs
